a0922cb6f2889216d34c22ee387ae7161d3d17ad
bohechaojidan <540372108@qq.com>
2018-05-03 15:34:13 +0800

    logo

--- src/views/layout/components/Sidebar/Index.vue
+++ src/views/layout/components/Sidebar/Index.vue
@@ -1,5 +1,10 @@
 <template>
   <scroll-bar>
+     <div class="logobox">
+      <img :src="imglogo" alt="">
+      <div class="chanyikeji fontnormal">河南科技大学毕设管理系统</div>
+      <!-- <i class="iconfont icon-ico_category"></i> -->
+    </div>
     <el-menu mode="vertical" :default-active="$route.path" height='100vh' :collapse="isCollapse" background-color="white" text-color="black" active-text-color="#409EFF">
       <sidebar-item :routes="permission_routers"></sidebar-item>
     </el-menu>
@@ -10,10 +15,13 @@
 import { mapGetters } from 'vuex'
 import SidebarItem from './SidebarItem'
 import ScrollBar from '@/components/ScrollBar'
+import imglogo from '@/assets/logo_images/logo.jpg'
 
 export default {
   data() {
-    return {}
+    return {
+      imglogo
+    }
   },
   components: { SidebarItem, ScrollBar },
   computed: {
