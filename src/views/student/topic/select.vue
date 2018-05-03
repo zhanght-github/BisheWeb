ff5d7f25109c55198c372babdb93fe31cb612670
mingming <897408187@qq.com>
2018-05-03 17:23:32 +0800

    前端学生模块

--- src/views/student/topic/select.vue
+++ src/views/student/topic/select.vue
@@ -4,29 +4,153 @@
       <el-table :data="tableData" border style="width: 100%" v-loading="loading">
         <el-table-column type="index" label="序号" width="50" align="center"></el-table-column>
         <el-table-column prop="topicname" label="课题名称" width="100" align="center"></el-table-column>
-        <el-table-column prop="topiccontent" label="选题内容" align="center"></el-table-column>
+        <el-table-column label="选题内容" align="left">
+          <template slot-scope="scope">
+            <div v-html="scope.row.topiccontent"></div>
+          </template>
+        </el-table-column>
         <el-table-column prop="topicstate" label="选题状态" width="120" align="center"></el-table-column>
-        <el-table-column prop="alreadynum" label="已选人数" width="120" align="center"></el-table-column>
         <el-table-column prop="supplynum" label="选题总人数" width="120" align="center"></el-table-column>
+        <el-table-column prop="alreadynum" label="已选人数" width="120" align="center"></el-table-column>
         <el-table-column prop="createtime" label="创建时间" width="200" align="center"></el-table-column>
         <el-table-column label="操作" width="200">
           <template slot-scope="scope" >
-            <el-button type="primary" size="small">查看</el-button>
-            <el-button type="success" size="small">选择</el-button>
+            <el-button type="primary" size="small" @click="handleOpen()">查看</el-button>
+            <el-button type="success" size="small" @click="handleSelect()">选择</el-button>
           </template>
         </el-table-column>
       </el-table>
       <!--<el-pagination-->
-        <!--background-->
-        <!--@size-change="handleSizeChange"-->
-        <!--@current-change="handleCurrentChange"-->
-        <!--:current-page="tableParams.pageNum"-->
-        <!--:page-sizes="[10, 20, 30]"-->
-        <!--:page-size="tableParams.pageSize"-->
-        <!--layout="total, sizes, prev, pager, next, jumper"-->
-        <!--:total="paginationTotal">-->
+      <!--background-->
+      <!--@size-change="handleSizeChange"-->
+      <!--@current-change="handleCurrentChange"-->
+      <!--:current-page="tableParams.pageNum"-->
+      <!--:page-sizes="[10, 20, 30]"-->
+      <!--:page-size="tableParams.pageSize"-->
+      <!--layout="total, sizes, prev, pager, next, jumper"-->
+      <!--:total="paginationTotal">-->
       <!--</el-pagination>-->
     </div>
+    <el-dialog title="查看课题信息" class='editdia' width="450px" :visible.sync="topicDialog" :close-on-press-escape='false' :close-on-click-modal='false'>
+      <div class="diabody diascroll" style="height: 400px">
+        <div class="detail-row">
+          <div class="row-title">课题名称</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">课题内容</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">显示排序</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">课题状态</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">选题总人数</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">已选人数</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">选题时间</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">指导教师</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">课题类型</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">课题来源</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">指导老师电话</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">日程安排</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">学院</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+        <div class="detail-row">
+          <div class="row-title">专业</div>
+          <div class="row-content">
+            <span class="input-wrapper">
+              <el-input></el-input>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="diafoot flex">
+        <el-button type="primary" class="truebutton deepbluebtn">确定</el-button>
+        <el-button type="info" class="cancelbtn" plain @click="closeDialog()">取消</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -39,6 +163,7 @@
         page: 1,
         size: 10,
         loading: false,
+        topicDialog:false,
         tableData: []
       }
     },
@@ -47,6 +172,28 @@
         topicSelect(this.page - 1, this.size).then(res => {
           this.tableData = res.data.data.content
         })
+      },
+      closeDialog(){
+        this.topicDialog = !this.topicDialog;
+      },
+      handleOpen(){
+        this.topicDialog = !this.topicDialog;
+      },
+      handleSelect(){
+        this.$confirm(`确定要删除「${name}」吗?`, '确定删除？', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+          customClass: 'blueMessage'
+        }).then(() => {
+          console.log('确认成功');
+        })
+          .catch(() => {
+            this.$message({
+              type: 'info',
+              message: '已取消删除'
+            })
+          })
       }
     },
     created() {
