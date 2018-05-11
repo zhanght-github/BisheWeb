f82231bc585f2c59667aeb6110b435ec40be2f45
mingming <897408187@qq.com>
2018-05-11 16:30:29 +0800

    11号下午前端

---
+++ src/views/student/greatReport/greatReport.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {},
+    created() {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
